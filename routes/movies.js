d05a4b7f3e0b64c50028493374ff191f987959e2
Nate <n8bdoesemailvia@gmail.com>
2017-09-14 14:38:26 -0600

    Alright, so...this time I added .js to to require statments in my routes file

--- routes/movies.js
+++ routes/movies.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('../db/knex');
-var queries = require('../db/queries')
+var knex = require('../db/knex.js');
+var queries = require('../db/queries.js')
 
 function validMovies(movies) {
   const hasTitle = typeof movies.Title == 'string' && movies.Title.trim() != '';
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('../db/knex');
-var queries = require('../db/queries')
+var knex = require('../db/knex.js');
+var queries = require('../db/queries.js')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
