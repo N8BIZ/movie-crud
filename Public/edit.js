b7e72841ab07f1b6e95a2e1bfdcdddbec5f34d32
Nate <n8bdoesemailvia@gmail.com>
2017-09-15 17:42:29 -0600

    new functions

--- Public/edit.js
+++ Public/edit.js
@@ -13,5 +13,23 @@ $(() => {
       $('#year').val(edit.Year);
       $('#myrating').val(edit.My_Rating);
       $('#editButton').attr('href', `home.html?id=${edit.id}`)
-})
-})
+
+      $('#editButton').click((event) => {
+        event.preventDefault();
+          $.ajax({
+            type: 'PUT',
+            dataType: 'json',
+            url: `${movieAPI}/${id}`,
+            data: {
+              url: $('#url').val(),
+              Title: $('#title').val(),
+              Director: $('#director').val(),
+              Year: $('#year').val(),
+              My_Rating: $('#myrating').val(),
+            },
+        }).then(result => {
+          window.location = `home.html?id=${id}`;
+        });
+      });
+    });
+});
