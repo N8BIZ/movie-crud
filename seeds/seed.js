deb0a8cb4464181ed59f0182103f13332934d665
Nate <n8bdoesemailvia@gmail.com>
2017-09-28 21:26:13 -0600

    Minor changes

--- seeds/seed.js
+++ seeds/seed.js
@@ -9,21 +9,21 @@ exports.seed = function(knex, Promise) {
         Director: "Steven Soderbergh",
         Year: 1976,
         My_Rating: 4,
-        url:"https://images-na.ssl-images-amazon.com/images/M/MV5BOTY3ODkzMDM2NF5BMl5BanBnXkFtZTgwNjI1NjIzMzI@._V1_SY1000_CR0,0,674,1000_AL_.jpg"
+        url:"https://images-na.ssl-images-amazon.com/images/M/MV5BMTc4NDQ5NjQ2MF5BMl5BanBnXkFtZTcwNzU3NDAwMQ@@._V1_UY268_CR4,0,182,268_AL_.jpg"
         },
         {
           Title: "Nashville",
           Director: "Robert Altman",
           Year: 1976,
           My_Rating: 5,
-          url: "https://images-na.ssl-images-amazon.com/images/M/MV5BOTY3ODkzMDM2NF5BMl5BanBnXkFtZTgwNjI1NjIzMzI@._V1_SY1000_CR0,0,674,1000_AL_.jpg"
+          url: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTEzNDE2Mjg0MTdeQTJeQWpwZ15BbWU4MDUxNTczNzAy._V1_UX182_CR0,0,182,268_AL_.jpg"
         },
         {
           Title: "Idiocracy",
           Director: "Mike Judge",
           Year: 2006,
           My_Rating: 5,
-          url: "https://images-na.ssl-images-amazon.com/images/M/MV5BOTY3ODkzMDM2NF5BMl5BanBnXkFtZTgwNjI1NjIzMzI@._V1_SY1000_CR0,0,674,1000_AL_.jpg"
+          url: "https://images-na.ssl-images-amazon.com/images/M/MV5BMWQ4MzI2ZDQtYjk3MS00ODdjLTkwN2QtOTBjYzIwM2RmNzgyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg"
         },
       ]);
     });
